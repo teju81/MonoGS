e84a46d7f8027229369377b79c9b9f041cf44bd7
teju81 <teju81@gmail.com>
2024-09-03 18:52:30 +0530

    Update slam_gui.py

--- monogs_ros/monogs_ros/gui/slam_gui.py
+++ monogs_ros/monogs_ros/gui/slam_gui.py
@@ -920,7 +920,7 @@ def main():
 
     params_gui = {}
 
-    config = "/monogs_ros_ws/src/monogs_ros/monogs_ros/configs/rgbd/tum/fr1_desk.yaml"
+    config_file = "/monogs_ros_ws/src/monogs_ros/monogs_ros/configs/rgbd/tum/fr1_desk.yaml"
 
     with open(config_file, "r") as yml:
         config = yaml.safe_load(yml)
